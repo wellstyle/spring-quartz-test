cc4fdb281e5678a2c1efb2dc1ffa8e1ab62f0ebe
yookeun <ykkim@andwise.com>
2015-10-15 17:04:30 +0900

    first commit

---
+++ src/main/java/test/MyJob1.java
@@ -0,0 +1,20 @@
+package test;
+
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.scheduling.quartz.QuartzJobBean;
+
+public class MyJob1 extends QuartzJobBean{
+	
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	protected void executeInternal(JobExecutionContext arg0) throws JobExecutionException {
+		// TODO Auto-generated method stub
+		System.out.println("MyJob1 were called :: " +name);
+	}
+}
---
+++ src/main/java/test/QuartzMain.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class QuartzMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 설정 xml를 읽어 들인다.
+				ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+	}
+
+}
